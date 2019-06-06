f9d141fb91f1ff5c863db719dcde77602e497cbf
Sraavan <sraavansridhar@gmail.com>
2019-06-06 14:38:42 -0400

    Updated base and socket url

--- public/src/app/Services/app-service/app.service.ts
+++ public/src/app/Services/app-service/app.service.ts
@@ -5,7 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class AppService {
 
-  baseUrl = 'http://localhost:3000';
+  // baseUrl = 'http://localhost:3000';
+  baseUrl = 'https://chat-room-alpha.herokuapp.com/';
 
-  socketUrl = 'http://localhost:3000';
+  // socketUrl = 'http://localhost:3000';
+  socketUrl = 'https://chat-room-alpha.herokuapp.com/';
 }
