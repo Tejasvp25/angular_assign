66d005ffd82ae9861e373c9f13b24660ee3a4444
Sraavan <sraavansridhar@gmail.com>
2019-06-06 01:43:32 -0400

    Cleaned up unused files

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 ## ChatRoom
 
-A web chat room  running a Node.js server in bluemix. The backend has been implemented with Express, MongoDB and Socket.io.
-
-
-[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)
-![Platform](https://img.shields.io/badge/platform-NODE-lightgrey.svg?style=flat)
+A simple chat application running on MEAN . The backend has been implemented with Express, MongoDB and Socket.io.
 
   
   
--- server/server.js
+++ server/server.js
@@ -1,19 +1,5 @@
-// Uncomment following to enable zipkin tracing, tailor to fit your network configuration:
-// var appzip = require('appmetrics-zipkin')({
-//     host: 'localhost',
-//     port: 9411,
-//     serviceName:'frontend'
-// });
-
-// require('appmetrics-dash').attach();
-// require('appmetrics-prometheus').attach();
-const appName = require('./../package').name;
-const log4js = require('log4js');
-
 var path = require('path')
 var socketManager = require('./controllers/SocketController')
-
-const logger = log4js.getLogger(appName);
 
 const app = require('./config/headers').app
 const server = require('./config/headers').server
@@ -27,20 +13,15 @@ mongoose.connect('mongodb://localhost/chatRoomDb', {
     useNewUrlParser: true
 })
 
-app.use(log4js.connectLogger(logger, { level: process.env.LOG_LEVEL || 'info' }));
-const serviceManager = require('./services/service-manager');
-require('./services/index')(app);
 require('./routers/index')(app, server);
 require('./routers/routes')(app);
 
 const port = process.env.PORT || localConfig.port;
 server.listen(port, function () {
-    logger.info(`ChatRoom listening on http://localhost:${port}/appmetrics-dash`);
-    logger.info(`ChatRoom listening on http://localhost:${port}`);
     console.log(`ChatRoom listening on http://localhost:${port}`);
 });
 
-// Let angular handle the routes
+// Pass route handling to angular
 app.use(function (req, res, next) {
     res.sendFile(path.join(__dirname, '../public/dist/chatroom/index.html'));
 })
